29fbd717207c54cf6b3432fc085c03d3ef718202
Ardusa <ardusa05@gmail.com>
2025-02-09 01:39:59 -0500

    Initial Commit

---
+++ TikTok Shuffle/TikTok_ShuffleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTok_ShuffleApp.swift
+//  TikTok Shuffle
+//
+//  Created by Ankur Desai on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct TikTok_ShuffleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TikTok ShuffleTests/TikTok_ShuffleTests.swift
@@ -0,0 +1,17 @@
+//
+//  TikTok_ShuffleTests.swift
+//  TikTok ShuffleTests
+//
+//  Created by Ankur Desai on 2/9/25.
+//
+
+import Testing
+@testable import TikTok_Shuffle
+
+struct TikTok_ShuffleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
